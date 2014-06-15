fe44ba5a5af62d71ed24f64d18d76255405d8547
Dmitry Demidov <dsdemidov@nulance.ru>
2014-06-15 20:56:29 +0400

    Passwd length added

--- PasswordGenerator.php
+++ PasswordGenerator.php
@@ -1,8 +1,20 @@
 <?php 
 class PasswordGenerator {
+	private $passwordLength = 8;
+
 	public function generate() {
-		$password = rand(5, 15);
+		$password = '';
+		for ($i = 0; $i < $this->getPasswordLength(); $i++ ) {
+			$password .= rand(0,9);
+		}
 		return $password;
+	}
+
+	public function getPasswordLength() {
+		return $this->passwordLength;
+	}
+	public function setPasswordLength( int $value ) {
+		$this->passwordLength = $value;
 	}
 }
 ?>
